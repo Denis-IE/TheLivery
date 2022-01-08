b8d4958e9cbde8986e8f89f6627cf2f10cc43a52
Denis Popan <35832629+DenisPopan@users.noreply.github.com>
2022-01-08 23:48:44 +0200

    Add project files.

---
+++ TheLivery/Pages/Comenzi/Create.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TheLivery.Data;
+using TheLivery.Models;
+
+namespace TheLivery.Pages.Comenzi
+{
+    public class CreateModel : PageModel
+    {
+        private readonly TheLivery.Data.CurieratContext _context;
+
+        public CreateModel(TheLivery.Data.CurieratContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["ColetID"] = new SelectList(_context.Set<Colet>(), "ID", "ID");
+        ViewData["CurierID"] = new SelectList(_context.Set<Curier>(), "ID", "ID");
+            return Page();
+        }
+
+        [BindProperty]
+        public Comanda Comanda { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Comenzi.Add(Comanda);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
